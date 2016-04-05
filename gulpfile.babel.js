501f2b1e510854c7b03a06dbe315f3eb9f49b9b6
Jesse Ruder <jesse@fretboard.io>
2016-04-05 14:02:45 -0700

    Fix gulp publish and publish version 0.0.19

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,10 +15,12 @@ gulp.task('build', tasks.babel);
 gulp.task('watch', gulp.series(tasks.babel, tasks.watchBabel));
 gulp.task('clean', tasks.clean);
 
-gulp.task('publish', gulp.parallel(
-  gulp.series(tasks.clean, tasks.babel),
-  tasks.archiveTemplate,
-  shell.task(['npm publish']),
+gulp.task('publish', gulp.series(
+  gulp.parallel(
+    gulp.series(tasks.clean, tasks.babel),
+    tasks.archiveTemplate
+  ),
+  shell.task(['npm publish'])
 ));
 
 gulp.task('default', gulp.series('watch'));
